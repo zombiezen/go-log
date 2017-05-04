7dfa96b70f81bcbc99fd81c08e690aff74aad70e
Ross Light <light@google.com>
2017-05-04 17:10:20 -0400

    testlog: new package

---
+++ testlog/example_test.go
@@ -0,0 +1,32 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testlog_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"zombiezen.com/go/log"
+	"zombiezen.com/go/log/testlog"
+)
+
+var (
+	m *testing.M
+	t *testing.T
+)
+
+func ExampleMain() {
+	// Inside your TestMain. m is a *testing.M.
+	testlog.Main(nil)
+	os.Exit(m.Run())
+}
+
+func ExampleWithTB() {
+	// Inside a test. t is a *testing.T.
+	// You must have set the default logger using Main or log.SetDefault.
+	ctx := testlog.WithTB(context.Background(), t)
+	log.Infof(ctx, "Log to *testing.T")
+}
---
+++ testlog/testlog.go
@@ -0,0 +1,64 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package testlog provides a Logger that writes to a *testing.T or *testing.B.
+// See the examples for how to set this up.
+package testlog
+
+import (
+	"context"
+
+	"zombiezen.com/go/log"
+)
+
+// Logger writes to a *testing.T or *testing.B that comes from the Context.
+type Logger struct {
+	// Fallback is the Logger used if the Context does not have a TB.
+	// If nil, then log.Discard is assumed.
+	Fallback log.Logger
+}
+
+// Main sets the default logger to a testlog.Logger. fallback may be nil.
+// Main is intended to be called in TestMain.
+func Main(fallback log.Logger) {
+	log.SetDefault(Logger{fallback})
+}
+
+// Log writes to the TB in ctx or l.Fallback.
+func (l Logger) Log(ctx context.Context, e log.Entry) {
+	tb, _ := ctx.Value(ctxKey{}).(TB)
+	if tb == nil {
+		if l.Fallback != nil {
+			l.Fallback.Log(ctx, e)
+		}
+		return
+	}
+	switch e.Level {
+	case log.Warn:
+		tb.Log("WARN: " + e.Msg)
+	case log.Error:
+		tb.Log("ERROR: " + e.Msg)
+	default:
+		tb.Log(e.Msg)
+	}
+}
+
+// LogEnabled always returns true.
+func (l Logger) LogEnabled(e log.Entry) bool {
+	return true
+}
+
+type ctxKey struct{}
+
+// WithTB returns a Context derived from parent that will use tb to log
+// when sending an entry to this package's Logger.
+func WithTB(parent context.Context, tb TB) context.Context {
+	return context.WithValue(parent, ctxKey{}, tb)
+}
+
+// TB is the interface provided by a *testing.T or a *testing.B
+// that is needed for Logger.
+type TB interface {
+	Log(...interface{})
+}
