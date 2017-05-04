16cbc2a2077ef85b60c04ed3583657dddc9ec919
Ross Light <light@google.com>
2017-05-04 09:53:14 -0400

    log: move foo into example test

---
+++ example_test.go
@@ -0,0 +1,28 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Sample application to demonstrate the log package.
+
+package log_test
+
+import (
+	"context"
+	"os"
+
+	"zombiezen.com/go/log"
+)
+
+func Example() {
+	// Initialize the global logger.
+	// This should only happen in main and before any log statements.
+	stderrLog := log.New(os.Stdout, "", 0, nil)
+	log.SetDefault(stderrLog)
+
+	// Once the logger is set, you can log from anywhere.
+	ctx := context.Background()
+	log.Infof(ctx, "Hello, World!")
+
+	// Output:
+	// Hello, World!
+}
