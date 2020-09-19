3de76a72668f516bce1beab7a40755f9d697904a
Ross Light <ross@zombiezen.com>
2020-09-18 21:05:34 -0700

    log: add code to trigger go vet

--- format.go
+++ format.go
@@ -15,26 +15,46 @@ import (
 
 // Infof writes an info message to the default Logger.  Its arguments are handled in the manner of fmt.Sprintf.
 func Infof(ctx context.Context, format string, args ...interface{}) {
+	if false {
+		// Enable printf checking in go vet.
+		_ = fmt.Sprintf(format, args...)
+	}
 	logf(ctx, Default(), Info, format, args)
 }
 
 // Debugf writes a debug message to the default Logger.  Its arguments are handled in the manner of fmt.Sprintf.
 func Debugf(ctx context.Context, format string, args ...interface{}) {
+	if false {
+		// Enable printf checking in go vet.
+		_ = fmt.Sprintf(format, args...)
+	}
 	logf(ctx, Default(), Debug, format, args)
 }
 
 // Warnf writes a warning message to the default Logger.  Its arguments are handled in the manner of fmt.Sprintf.
 func Warnf(ctx context.Context, format string, args ...interface{}) {
+	if false {
+		// Enable printf checking in go vet.
+		_ = fmt.Sprintf(format, args...)
+	}
 	logf(ctx, Default(), Warn, format, args)
 }
 
 // Errorf writes an error message to the default Logger.  Its arguments are handled in the manner of fmt.Sprintf.
 func Errorf(ctx context.Context, format string, args ...interface{}) {
+	if false {
+		// Enable printf checking in go vet.
+		_ = fmt.Sprintf(format, args...)
+	}
 	logf(ctx, Default(), Error, format, args)
 }
 
 // Logf writes a message to a Logger.  Its arguments are handled in the manner of fmt.Sprintf.
 func Logf(ctx context.Context, logger Logger, level Level, format string, args ...interface{}) {
+	if false {
+		// Enable printf checking in go vet.
+		_ = fmt.Sprintf(format, args...)
+	}
 	logf(ctx, logger, level, format, args)
 }
 
